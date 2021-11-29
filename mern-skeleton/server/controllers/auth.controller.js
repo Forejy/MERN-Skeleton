240cb22b6de64a716744575173df5e1111a188c4
Forejy <joffrey.marques@gmail.com>
2021-11-29 22:41:10 +0100

    Début de 'apprendre à utiliser postman'

--- mern-skeleton/server/controllers/auth.controller.js
+++ mern-skeleton/server/controllers/auth.controller.js
@@ -22,7 +22,7 @@ const signin = async (req, res) => {
     const token = jwt.sign({ //jwt c'est l'équivalent d'un filigrane qui permet de vérifier qu'un passeport  ou cart d'identité est une vraie mais ici je sais pas à quoi ça sert exactement, je sais juste que ça va etre stocké dans le client comme un cookie
       //Avec JWT, stocker le state de l'utilisateur c'est la responsabilité du client
     //Quand l'utilisateur signout, le client a besoin de supprimer le token coté client pour que l'utilisateur ne soit plus authentifié.
-    //Coté serveur, on peut verifier le token généré lorsqu'un utilisateur se //connecte  pour protéger les routes qui ont besoin de l'etre et d'avoir une authentifaction valide.
+    //Coté serveur, on peut verifier le token généré lorsqu'un utilisateur se connecte  pour protéger les routes qui ont besoin de l'etre et d'avoir une authentifaction valide.
       _id: user._id
     }, config.jwtSecret)
 
@@ -56,12 +56,17 @@ const signout = (req, res) => {
 }
 
 
-
+//expressJwt (requireSignin) sert à vérifier que le Jwt est valide
+  // Si le token du jwt est valide expressJwt va append l'id de l'utilisateur (alors vérifié) dans un objet 'auth' dans l'object req(uest)
+  //Sinon le module expressJwt throw une erreur
 const requireSignin = expressJwt({
   secret: config.jwtSecret,
   algorithms: ['HS256'],
   userProperty: 'auth'
 })
+
+//hasAuthorization vérifier si l'utilisateur (alors authentitifé) est le meme que l'utilisateur dont on demande la mise à jour ou dont on demande la suppression
+//req.auth est crée par expressJwt lors de la verification de l'authentitifaction (avant chaque )
 
 const hasAuthorization = (req, res, next) => {
   const authorized = req.profile && req.auth && req.profile._id == req.auth._id
@@ -72,6 +77,7 @@ const hasAuthorization = (req, res, next) => {
   }
   next()
 }
+
 
 export default {
   signin,
